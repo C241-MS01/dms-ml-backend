c336d70d1ea647bde661b051e4343250bad08c7d
Bagas Hizbullah <work.bitsy@simplelogin.com>
2024-05-27 16:58:00 +0000

    feat: implement mqtt with flask

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
 from flask import Flask
+from flask_mqtt import Mqtt
+from dotenv import load_dotenv
+from structlog import get_logger
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config["HTTP_URL"] = os.getenv("HTTP_URL")
+app.config["HTTP_PORT"] = os.getenv("HTTP_PORT")
+app.config["MQTT_BROKER_URL"] = os.getenv("MQTT_BROKER_URL")
+app.config["MQTT_BROKER_PORT"] = int(os.getenv("MQTT_BROKER_PORT"))
+app.logger = get_logger()
+mqtt_client = Mqtt(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@mqtt_client.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    if rc != 0:
+        print(f"Connection failed with result code {rc}")
+        return
+
+    app.logger.info(f"Connected to MQTT broker at {app.config['MQTT_BROKER_URL']}:{app.config['MQTT_BROKER_PORT']}")
+
+    mqtt_client.subscribe("video")
+    app.logger.info("Subscribed to topic 'video'")
+
+
+@mqtt_client.on_message()
+def handle_message(client, userdata, message):
+    app.logger.info(f"Received message: {message.payload.decode()}")
+
+def main():
+    app.run(host=app.config["HTTP_URL"], port=app.config["HTTP_PORT"])
+
+if __name__ == "__main__":
+    main()
