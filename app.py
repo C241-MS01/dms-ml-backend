65dd6b5b3e6f0ae1d3ace78312ef75745ced7105
Bagas Hizbullah <work.bitsy@simplelogin.com>
2024-05-29 14:17:50 +0000

    feat: add face mesh opencv

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_mqtt import Mqtt
 from dotenv import load_dotenv
 from structlog import get_logger
@@ -14,13 +14,16 @@ app.config["MQTT_BROKER_PORT"] = int(os.getenv("MQTT_BROKER_PORT"))
 app.logger = get_logger()
 mqtt_client = Mqtt(app)
 
+
 @mqtt_client.on_connect()
 def handle_connect(client, userdata, flags, rc):
     if rc != 0:
         print(f"Connection failed with result code {rc}")
         return
 
-    app.logger.info(f"Connected to MQTT broker at {app.config['MQTT_BROKER_URL']}:{app.config['MQTT_BROKER_PORT']}")
+    app.logger.info(
+        f"Connected to MQTT broker at {app.config['MQTT_BROKER_URL']}:{app.config['MQTT_BROKER_PORT']}"
+    )
 
     mqtt_client.subscribe("video")
     app.logger.info("Subscribed to topic 'video'")
@@ -30,8 +33,21 @@ def handle_connect(client, userdata, flags, rc):
 def handle_message(client, userdata, message):
     app.logger.info(f"Received message: {message.payload.decode()}")
 
+
+# Video stream test
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/stream")
+def stream():
+    return render_template("streamer.html")
+
+
 def main():
     app.run(host=app.config["HTTP_URL"], port=app.config["HTTP_PORT"])
+
 
 if __name__ == "__main__":
     main()
